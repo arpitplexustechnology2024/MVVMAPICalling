ff6131b1718253f55fc1d41190ddf6486ce2363c
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-06-14 12:16:58 -0700

    Initial Commit

---
+++ MVVMAPICalling/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMAPICalling
+//
+//  Created by Arpit iOS Dev. on 14/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
